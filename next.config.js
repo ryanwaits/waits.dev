d47810ae79927a9d7824ce35d17eaa7998aa8e85
Ryan Waits <ryan.waits@gmail.com>
2024-02-15 09:02:26 -0600

    update with more examples

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 const { remarkCodeHike } = require("@code-hike/mdx")
+const theme = require('./lib/theme.json');
 
 const withNextra = require("nextra")({
   theme: "nextra-theme-blog",
   themeConfig: "./theme.config.js",
   mdxOptions: {
-    remarkPlugins: [[remarkCodeHike, { theme: "poimandres" }]],
+    remarkPlugins: [[remarkCodeHike, { theme }]],
   },
   pageExtensions: ["js", "jsx", "md", "mdx"],
 })
